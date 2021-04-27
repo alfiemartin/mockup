a720bfe9ab633f6b0d064089022cdb3b117f07d3
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-27 15:33:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+// import sideImage from "../public/";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideContainer">
+        <img src="/thumbnail_Sideimage.jpg" alt="" className="logo" />
+      </div>
+      <main className="signupContainer">
+        <img src="/thumbnail_Logo.png" alt="" />
+        <form action="">
+          <div className="emailContainer">
+            <label htmlFor="" className="emailLabel">
+              Email
+            </label>
+            <input type="text" />
+          </div>
+          <div className="passwordLabelContainer">
+            <label htmlFor="" className="passwordLabel">
+              Password
+            </label>
+            <label htmlFor="" className="forgotPasswordLabel">
+              Forgot Password?
+            </label>
+          </div>
+          <input type="password" />
+          <button className="login">
+            <span>Login</span>
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
